787b0f3804a63624e9684912d01a0344cc02e4f8
takeshix <takeshix@adversec.com>
2020-07-26 16:16:01 +0200

    Updated types.go

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -6,22 +6,21 @@ import (
 )
 
 type PluginConstructor func() DeenPlugin
-type FuncStub func([]byte) []byte
-type StreamFuncStub func(io.Reader) ([]byte, error)
 type AddCliFuncStub func(*DeenPlugin, []string) *flag.FlagSet
-type FuncWithCliFlagsStub func(*flag.FlagSet, []byte) ([]byte, error)
+
+type StreamFuncStub func(io.Reader) ([]byte, error)
 type StreamFuncWithCliFlagsStub func(*flag.FlagSet, io.Reader) ([]byte, error)
+
+type PipeFuncStub func(io.Reader, *io.Writer) error
+type PipeFuncWithCLIFlagsStub func(*flag.FlagSet, io.Reader, *io.Writer) error
 
 type DeenPlugin struct {
 	Name                            string
 	Aliases                         []string
 	Type                            string
 	Unprocess                       bool
-	ProcessFunc                     FuncStub
-	UnprocessFunc                   FuncStub
 	ProcessStreamFunc               StreamFuncStub
 	UnprocessStreamFunc             StreamFuncStub
-	ProcessWithCliFlagsFunc         FuncWithCliFlagsStub
 	ProcessStreamWithCliFlagsFunc   StreamFuncWithCliFlagsStub
 	UnprocessStreamWithCliFlagsFunc StreamFuncWithCliFlagsStub
 	AddCliOptionsFunc               AddCliFuncStub
