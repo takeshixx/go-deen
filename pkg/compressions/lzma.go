c3a8693f959435f170d71f5830405fe9b955285e
takeshix <takeshix@adversec.com>
2020-08-23 15:30:44 +0200

    Updated lzma and lzma2 plugins

--- pkg/compressions/lzma.go
+++ pkg/compressions/lzma.go
@@ -1,8 +1,10 @@
 package compressions
 
 import (
-	"bytes"
+	"flag"
+	"fmt"
 	"io"
+	"os"
 
 	"github.com/ulikunitz/xz/lzma"
 
@@ -15,31 +17,50 @@ func NewPluginLZMA() (p types.DeenPlugin) {
 	p.Aliases = []string{".lzma"}
 	p.Type = "compression"
 	p.Unprocess = false
-	p.ProcessStreamFunc = func(reader io.Reader) ([]byte, error) {
-		var outBuf bytes.Buffer
-		var err error
-		compressor, err := lzma.NewWriter(&outBuf)
-		if err != nil {
-			return outBuf.Bytes(), err
-		}
-		if _, err := io.Copy(compressor, reader); err != nil {
-			return outBuf.Bytes(), err
-		}
-		compressor.Close()
-		return outBuf.Bytes(), err
+	p.ProcessDeenTaskFunc = func(task *types.DeenTask) {
+		go func() {
+			defer task.Close()
+			compressor, err := lzma.NewWriter(task.PipeWriter)
+			if err != nil {
+				task.ErrChan <- err
+			}
+			if _, err := io.Copy(compressor, task.Reader); err != nil {
+				task.ErrChan <- err
+			}
+			err = compressor.Close()
+			if err != nil {
+				task.ErrChan <- err
+			}
+		}()
 	}
-	p.UnprocessStreamFunc = func(reader io.Reader) ([]byte, error) {
-		var outBuf bytes.Buffer
-		var err error
-		decompressor, err := lzma.NewReader(reader)
-		if err != nil {
-			return outBuf.Bytes(), err
+	p.ProcessDeenTaskWithFlags = func(flags *flag.FlagSet, task *types.DeenTask) {
+		p.ProcessDeenTaskFunc(task)
+	}
+	p.UnprocessDeenTaskFunc = func(task *types.DeenTask) {
+		go func() {
+			defer task.Close()
+			decompressor, err := lzma.NewReader(task.Reader)
+			if err != nil {
+				task.ErrChan <- err
+			}
+			_, err = io.Copy(task.PipeWriter, decompressor)
+			if err != nil {
+				task.ErrChan <- err
+			}
+		}()
+	}
+	p.UnprocessDeenTaskWithFlags = func(flags *flag.FlagSet, task *types.DeenTask) {
+		p.UnprocessDeenTaskFunc(task)
+	}
+	p.AddDefaultCliFunc = func(self *types.DeenPlugin, flags *flag.FlagSet, args []string) *flag.FlagSet {
+		flags.Init(p.Name, flag.ExitOnError)
+		flags.Usage = func() {
+			fmt.Fprintf(os.Stderr, "Usage of %s:\n\n", p.Name)
+			fmt.Fprintf(os.Stderr, "Decoding and encoding of LZMA streams.\n\n")
+			flags.PrintDefaults()
 		}
-		wrapper := struct{ io.Writer }{&outBuf}
-		if _, err := io.Copy(wrapper, decompressor); err != nil {
-			return outBuf.Bytes(), err
-		}
-		return outBuf.Bytes(), err
+		flags.Parse(args)
+		return flags
 	}
 	return
 }
@@ -50,32 +71,50 @@ func NewPluginLZMA2() (p types.DeenPlugin) {
 	p.Aliases = []string{".lzma2"}
 	p.Type = "compression"
 	p.Unprocess = false
-	p.ProcessStreamFunc = func(reader io.Reader) ([]byte, error) {
-		var outBuf bytes.Buffer
-		var err error
-		compressor, err := lzma.NewWriter2(&outBuf)
-		if err != nil {
-			return outBuf.Bytes(), err
-		}
-		if _, err := io.Copy(compressor, reader); err != nil {
-			return outBuf.Bytes(), err
-		}
-		// TODO: maybe always close the writer?
-		compressor.Close()
-		return outBuf.Bytes(), err
+	p.ProcessDeenTaskFunc = func(task *types.DeenTask) {
+		go func() {
+			defer task.Close()
+			compressor, err := lzma.NewWriter2(task.PipeWriter)
+			if err != nil {
+				task.ErrChan <- err
+			}
+			if _, err := io.Copy(compressor, task.Reader); err != nil {
+				task.ErrChan <- err
+			}
+			err = compressor.Close()
+			if err != nil {
+				task.ErrChan <- err
+			}
+		}()
 	}
-	p.UnprocessStreamFunc = func(reader io.Reader) ([]byte, error) {
-		var outBuf bytes.Buffer
-		var err error
-		decompressor, err := lzma.NewReader2(reader)
-		if err != nil {
-			return outBuf.Bytes(), err
+	p.ProcessDeenTaskWithFlags = func(flags *flag.FlagSet, task *types.DeenTask) {
+		p.ProcessDeenTaskFunc(task)
+	}
+	p.UnprocessDeenTaskFunc = func(task *types.DeenTask) {
+		go func() {
+			defer task.Close()
+			decompressor, err := lzma.NewReader2(task.Reader)
+			if err != nil {
+				task.ErrChan <- err
+			}
+			_, err = io.Copy(task.PipeWriter, decompressor)
+			if err != nil {
+				task.ErrChan <- err
+			}
+		}()
+	}
+	p.UnprocessDeenTaskWithFlags = func(flags *flag.FlagSet, task *types.DeenTask) {
+		p.UnprocessDeenTaskFunc(task)
+	}
+	p.AddDefaultCliFunc = func(self *types.DeenPlugin, flags *flag.FlagSet, args []string) *flag.FlagSet {
+		flags.Init(p.Name, flag.ExitOnError)
+		flags.Usage = func() {
+			fmt.Fprintf(os.Stderr, "Usage of %s:\n\n", p.Name)
+			fmt.Fprintf(os.Stderr, "Decoding and encoding of LZMA2 streams.\n\n")
+			flags.PrintDefaults()
 		}
-		wrapper := struct{ io.Writer }{&outBuf}
-		if _, err := io.Copy(wrapper, decompressor); err != nil {
-			return outBuf.Bytes(), err
-		}
-		return outBuf.Bytes(), err
+		flags.Parse(args)
+		return flags
 	}
 	return
 }
--- pkg/compressions/lzma_test.go
+++ pkg/compressions/lzma_test.go
@@ -1,11 +1,113 @@
 package compressions
 
-import "testing"
+import (
+	"bytes"
+	"encoding/hex"
+	"os"
+	"testing"
 
-func TestPluginLZMAProcess(t *testing.T) {
+	"github.com/takeshixx/deen/pkg/helpers"
+	"github.com/takeshixx/deen/pkg/types"
+)
 
+var lzmaTestData = []byte("deenLzMa data onlyUUUSEfor Testing PURPOSES")
+var lzmaTestDataCompressed = "5d00008000ffffffffffffffff0032196cdc4c8bd28be7a9dfcdd6221406244c46751fdf763f23209273abaaae7c90b600db9c28ab9958805e4cb94bcffffcf27000"
+var lzma2TestDataCompressed = "01002a6465656e4c7a4d612064617461206f6e6c795555555345666f722054657374696e6720505552504f53455300"
+
+func TestPluginLZMAProcessDeenTaskFunc(t *testing.T) {
+	destWriter := new(bytes.Buffer)
+	task := types.NewDeenTask(destWriter)
+	task.Reader = bytes.NewReader(lzmaTestData)
+	plugin := NewPluginLZMA()
+	plugin.ProcessDeenTaskFunc(task)
+	compressedData, err := hex.DecodeString(lzmaTestDataCompressed)
+	if err != nil {
+		t.Error(err)
+	}
+	select {
+	case err := <-task.ErrChan:
+		t.Error(err)
+	case <-task.DoneChan:
+		if c := bytes.Compare(destWriter.Bytes(), compressedData); c != 0 {
+			t.Errorf("TestPluginLZMAProcessDeenTaskFunc data wrong: %s != %s", hex.EncodeToString(destWriter.Bytes()), lzmaTestDataCompressed)
+		}
+	}
 }
 
-func TestPluginLZMAUnprocess(t *testing.T) {
+func TestPluginLZMAUnprocessDeenTaskFunc(t *testing.T) {
+	destWriter := new(bytes.Buffer)
+	task := types.NewDeenTask(destWriter)
+	compressedData, err := hex.DecodeString(lzmaTestDataCompressed)
+	if err != nil {
+		t.Error(err)
+	}
+	task.Reader = bytes.NewReader(compressedData)
+	plugin := NewPluginLZMA()
+	plugin.UnprocessDeenTaskFunc(task)
+	select {
+	case err := <-task.ErrChan:
+		t.Error(err)
+	case <-task.DoneChan:
+		if c := bytes.Compare(destWriter.Bytes(), []byte(lzmaTestData)); c != 0 {
+			t.Errorf("TestPluginLZMAUnprocessDeenTaskFunc data wrong: %s != %s", hex.EncodeToString(destWriter.Bytes()), lzmaTestData)
+		}
+	}
+}
 
+func TestPluginLZMA2ProcessDeenTaskFunc(t *testing.T) {
+	destWriter := new(bytes.Buffer)
+	task := types.NewDeenTask(destWriter)
+	task.Reader = bytes.NewReader(lzmaTestData)
+	plugin := NewPluginLZMA2()
+	plugin.ProcessDeenTaskFunc(task)
+	compressedData, err := hex.DecodeString(lzma2TestDataCompressed)
+	if err != nil {
+		t.Error(err)
+	}
+	select {
+	case err := <-task.ErrChan:
+		t.Error(err)
+	case <-task.DoneChan:
+		if c := bytes.Compare(destWriter.Bytes(), compressedData); c != 0 {
+			t.Errorf("TestPluginLZMA2ProcessDeenTaskFunc data wrong: %s != %s", hex.EncodeToString(destWriter.Bytes()), lzma2TestDataCompressed)
+		}
+	}
+}
+
+func TestPluginLZMA2UnprocessDeenTaskFunc(t *testing.T) {
+	destWriter := new(bytes.Buffer)
+	task := types.NewDeenTask(destWriter)
+	compressedData, err := hex.DecodeString(lzma2TestDataCompressed)
+	if err != nil {
+		t.Error(err)
+	}
+	task.Reader = bytes.NewReader(compressedData)
+	plugin := NewPluginLZMA2()
+	plugin.UnprocessDeenTaskFunc(task)
+	select {
+	case err := <-task.ErrChan:
+		t.Error(err)
+	case <-task.DoneChan:
+		if c := bytes.Compare(destWriter.Bytes(), []byte(lzmaTestData)); c != 0 {
+			t.Errorf("TestPluginLZMA2UnprocessDeenTaskFunc data wrong: %s != %s", hex.EncodeToString(destWriter.Bytes()), lzmaTestData)
+		}
+	}
+}
+
+func TestPluginLZMAAddDefaultCliFunc(t *testing.T) {
+	_, w, err := os.Pipe()
+	if err != nil {
+		t.Error(err)
+	}
+	os.Stderr = w
+
+	p := NewPluginLZMA()
+	flags := helpers.DefaultFlagSet()
+	flags = p.AddDefaultCliFunc(&p, flags, []string{})
+	flags.Usage()
+
+	p = NewPluginLZMA2()
+	flags = helpers.DefaultFlagSet()
+	flags = p.AddDefaultCliFunc(&p, flags, []string{})
+	flags.Usage()
 }
