7008fede29e443fb91de9375ffafac63c85d8607
takeshix <takeshix@adversec.com>
2020-08-23 14:13:48 +0200

    Updated flate plugin

--- pkg/compressions/flate_test.go
+++ pkg/compressions/flate_test.go
@@ -3,26 +3,109 @@ package compressions
 import (
 	"bytes"
 	"encoding/hex"
+	"os"
 	"testing"
+
+	"github.com/takeshixx/deen/pkg/helpers"
+	"github.com/takeshixx/deen/pkg/types"
 )
 
-var flateTestData = []byte("deenblaketest\xbd\xb2\x3d\xbc\x20\xe2\x8c\x98")
-var deflatedData = "789c4b494dcd4bca49cc4e2d492d2ed9bbc9768fc2a39e1900646309ea"
+var flateTestData = []byte("deenflatetestdata123123192840912atestflatedeenflatedeentest")
+var deflatedData = "4a494dcd4bcb492c492d492d2e49492c4934343206214b230b13034b43a344903858015c2588011205040000ffff"
+var deflateDataLevel1 = "04c0b10dc0300844d195024e118f7312df9545c3edafbc823e57c68c4b56e48a5cb1f37b9f1d29333e57a6a0cf95296833fe030000ffff"
 
-func TestPluginDeflateProcessStreamFunc(t *testing.T) {
+func TestPluginFlateProcessDeenTaskFunc(t *testing.T) {
+	destWriter := new(bytes.Buffer)
+	task := types.NewDeenTask(destWriter)
+	task.Reader = bytes.NewReader(flateTestData)
+	plugin := NewPluginFlate()
+	plugin.ProcessDeenTaskFunc(task)
+	compressedData, err := hex.DecodeString(deflatedData)
+	if err != nil {
+		t.Error(err)
+	}
+	select {
+	case err := <-task.ErrChan:
+		t.Error(err)
+	case <-task.DoneChan:
+		if c := bytes.Compare(destWriter.Bytes(), compressedData); c != 0 {
+			t.Errorf("TestPluginFlateProcessDeenTaskFunc data wrong: %s != %s", hex.EncodeToString(destWriter.Bytes()), deflatedData)
+		}
+	}
+}
+
+func TestPluginFlateProcessDeenTaskWithFlags(t *testing.T) {
+	destWriter := new(bytes.Buffer)
+	task := types.NewDeenTask(destWriter)
+	task.Reader = bytes.NewReader(flateTestData)
+	plugin := NewPluginFlate()
+	flags := helpers.DefaultFlagSet()
+	flags = plugin.AddDefaultCliFunc(&plugin, flags, []string{"-level", "1"})
+	plugin.ProcessDeenTaskWithFlags(flags, task)
+	compressedData, err := hex.DecodeString(deflateDataLevel1)
+	if err != nil {
+		t.Error(err)
+	}
+	select {
+	case err := <-task.ErrChan:
+		t.Error(err)
+	case <-task.DoneChan:
+		if c := bytes.Compare(destWriter.Bytes(), compressedData); c != 0 {
+			t.Errorf("TestPluginFlateProcessDeenTaskWithFlags data wrong: %s != %s", hex.EncodeToString(destWriter.Bytes()), deflateDataLevel1)
+		}
+	}
+}
+
+func TestPluginFlateUnprocessDeenTaskFunc(t *testing.T) {
+	destWriter := new(bytes.Buffer)
+	task := types.NewDeenTask(destWriter)
+	compressedData, err := hex.DecodeString(deflatedData)
+	if err != nil {
+		t.Error(err)
+	}
+	task.Reader = bytes.NewReader(compressedData)
+	plugin := NewPluginFlate()
+	plugin.UnprocessDeenTaskFunc(task)
+	select {
+	case err := <-task.ErrChan:
+		t.Error(err)
+	case <-task.DoneChan:
+		if c := bytes.Compare(destWriter.Bytes(), []byte(flateTestData)); c != 0 {
+			t.Errorf("TestPluginFlateUnprocessDeenTaskFunc data wrong: %s != %s", hex.EncodeToString(destWriter.Bytes()), flateTestData)
+		}
+	}
+}
+
+func TestPluginFlateUnprocessDeenTaskWithFlags(t *testing.T) {
+	destWriter := new(bytes.Buffer)
+	task := types.NewDeenTask(destWriter)
+	compressedData, err := hex.DecodeString(deflatedData)
+	if err != nil {
+		t.Error(err)
+	}
+	task.Reader = bytes.NewReader(compressedData)
+	plugin := NewPluginFlate()
+	flags := helpers.DefaultFlagSet()
+	flags = plugin.AddDefaultCliFunc(&plugin, flags, []string{})
+	plugin.UnprocessDeenTaskWithFlags(flags, task)
+	select {
+	case err := <-task.ErrChan:
+		t.Error(err)
+	case <-task.DoneChan:
+		if c := bytes.Compare(destWriter.Bytes(), []byte(flateTestData)); c != 0 {
+			t.Errorf("TestPluginFlateUnprocessDeenTaskFunc data wrong: %s != %s", hex.EncodeToString(destWriter.Bytes()), flateTestData)
+		}
+	}
+}
+
+func TestPluginFlateAddDefaultCliFunc(t *testing.T) {
 	p := NewPluginFlate()
-	r := bytes.NewReader(flateTestData)
-	d, e := p.ProcessStreamFunc(r)
-	if e != nil {
-		t.Errorf("DeflateProcessStreamFunc failed: %s", e)
+	flags := helpers.DefaultFlagSet()
+	flags = p.AddDefaultCliFunc(&p, flags, []string{})
+	_, w, err := os.Pipe()
+	if err != nil {
+		t.Error(err)
 	}
-	decoded, e := hex.DecodeString(deflatedData)
-	if e != nil {
-		t.Errorf("Decoding test data failed: %s", e)
-	}
-	t.Log(d)
-	t.Log(decoded)
-	if c := bytes.Compare(d, decoded); c != 0 {
-		t.Errorf("DeflateProcessSteamFunc returned invalid data: %s", d)
-	}
+	os.Stderr = w
+	flags.Usage()
 }
