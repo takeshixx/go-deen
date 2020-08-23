dfcf3c3e1d5ea893e33e984c3366ab84cc3ef221
takeshix <takeshix@adversec.com>
2020-08-23 15:12:42 +0200

    Updated gzip plugin

--- pkg/compressions/gzip.go
+++ pkg/compressions/gzip.go
@@ -1,14 +1,30 @@
 package compressions
 
 import (
-	"bytes"
 	"compress/gzip"
 	"flag"
 	"fmt"
 	"io"
+	"os"
+	"strconv"
 
 	"github.com/takeshixx/deen/pkg/types"
 )
+
+func doGzipCompress(task *types.DeenTask, level int) {
+	defer task.Close()
+	compressor, err := gzip.NewWriterLevel(task.PipeWriter, level)
+	if err != nil {
+		task.ErrChan <- err
+	}
+	if _, err := io.Copy(compressor, task.Reader); err != nil {
+		task.ErrChan <- err
+	}
+	err = compressor.Close()
+	if err != nil {
+		task.ErrChan <- err
+	}
+}
 
 // NewPluginGzip creates a new zlib plugin
 func NewPluginGzip() (p types.DeenPlugin) {
@@ -16,39 +32,49 @@ func NewPluginGzip() (p types.DeenPlugin) {
 	p.Aliases = []string{".gzip"}
 	p.Type = "compression"
 	p.Unprocess = false
-	p.ProcessStreamFunc = func(reader io.Reader) ([]byte, error) {
-		var outBuf bytes.Buffer
-		var err error
-		compressor := gzip.NewWriter(&outBuf)
-		if _, err := io.Copy(compressor, reader); err != nil {
-			return outBuf.Bytes(), err
-		}
-		compressor.Close()
-		return outBuf.Bytes(), err
+	p.ProcessDeenTaskFunc = func(task *types.DeenTask) {
+		doGzipCompress(task, gzip.DefaultCompression)
 	}
-	p.UnprocessStreamFunc = func(reader io.Reader) ([]byte, error) {
-		var outBuf bytes.Buffer
-		var err error
-		decompressor, err := gzip.NewReader(reader)
+	p.ProcessDeenTaskWithFlags = func(flags *flag.FlagSet, task *types.DeenTask) {
+		compressionLevel := gzip.DefaultCompression
+		level := flags.Lookup("level")
+		cliLevel, err := strconv.Atoi(level.Value.String())
 		if err != nil {
-			return outBuf.Bytes(), err
+			task.ErrChan <- err
 		}
-		wrapper := struct{ io.Writer }{&outBuf}
-		if _, err := io.Copy(wrapper, decompressor); err != nil {
-			return outBuf.Bytes(), err
+		if cliLevel >= -1 || cliLevel < 10 {
+			compressionLevel = cliLevel
 		}
-		decompressor.Close()
-		return outBuf.Bytes(), err
+		doGzipCompress(task, compressionLevel)
 	}
-	p.AddCliOptionsFunc = func(self *types.DeenPlugin, args []string) *flag.FlagSet {
-		gzipCmd := flag.NewFlagSet(p.Name, flag.ExitOnError)
-		gzipCmd.Usage = func() {
-			fmt.Printf("Usage of %s:\n\n", p.Name)
-			fmt.Printf("Implements reading and writing of gzip format compressed files (RFC1952).\n\n")
-			gzipCmd.PrintDefaults()
+	p.UnprocessDeenTaskFunc = func(task *types.DeenTask) {
+		defer task.Close()
+		wrappedReader := types.TrimReader{}
+		wrappedReader.Rd = task.Reader
+		decompressor, err := gzip.NewReader(wrappedReader)
+		if err != nil {
+			task.ErrChan <- err
 		}
-		gzipCmd.Parse(args)
-		return gzipCmd
+		_, err = io.Copy(task.PipeWriter, decompressor)
+		if err != nil {
+			task.ErrChan <- err
+		}
+	}
+	p.UnprocessDeenTaskWithFlags = func(flags *flag.FlagSet, task *types.DeenTask) {
+		p.UnprocessDeenTaskFunc(task)
+	}
+	p.AddDefaultCliFunc = func(self *types.DeenPlugin, flags *flag.FlagSet, args []string) *flag.FlagSet {
+		flags.Init(p.Name, flag.ExitOnError)
+		flags.Usage = func() {
+			fmt.Fprintf(os.Stderr, "Usage of %s:\n\n", p.Name)
+			fmt.Fprintf(os.Stderr, "Implements reading and writing of gzip format compressed files (RFC1952).\n\n")
+			flags.PrintDefaults()
+		}
+		if !self.Unprocess {
+			flags.Int("level", gzip.DefaultCompression, "compression level from 1 (best speed) to 9 (best compression)")
+		}
+		flags.Parse(args)
+		return flags
 	}
 	return
 }
