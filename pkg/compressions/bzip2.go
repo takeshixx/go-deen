5ab6a8c8c0b79753b89f713c49893887d75d5bec
takeshix <takeshix@adversec.com>
2020-08-24 09:39:32 +0200

    Updated bzip2 plugin

--- pkg/compressions/bzip2.go
+++ pkg/compressions/bzip2.go
@@ -1,13 +1,33 @@
 package compressions
 
 import (
-	"bytes"
-	"compress/bzip2"
 	"errors"
+	"flag"
+	"fmt"
 	"io"
+	"os"
+	"strconv"
 
+	"github.com/dsnet/compress/bzip2"
 	"github.com/takeshixx/deen/pkg/types"
 )
+
+func doBZip2Compress(task *types.DeenTask, level int) {
+	defer task.Close()
+	config := &bzip2.WriterConfig{Level: level}
+	compressor, err := bzip2.NewWriter(task.PipeWriter, config)
+	if err != nil {
+		task.ErrChan <- err
+	}
+	_, err = io.Copy(compressor, task.Reader)
+	if err != nil {
+		task.ErrChan <- err
+	}
+	err = compressor.Close()
+	if err != nil {
+		task.ErrChan <- err
+	}
+}
 
 // NewPluginBzip2 creates a new zlib plugin
 func NewPluginBzip2() (p types.DeenPlugin) {
@@ -15,18 +35,48 @@ func NewPluginBzip2() (p types.DeenPlugin) {
 	p.Aliases = []string{".bzip2"}
 	p.Type = "compression"
 	p.Unprocess = false
-	p.ProcessStreamFunc = func(reader io.Reader) ([]byte, error) {
-		return []byte{}, errors.New("Bzip2Process not implemented")
+	p.ProcessDeenTaskFunc = func(task *types.DeenTask) {
+		doBZip2Compress(task, bzip2.DefaultCompression)
 	}
-	p.UnprocessStreamFunc = func(reader io.Reader) ([]byte, error) {
-		var outBuf bytes.Buffer
-		var err error
-		decompressor := bzip2.NewReader(reader)
-		wrapper := struct{ io.Writer }{&outBuf}
-		if _, err := io.Copy(wrapper, decompressor); err != nil {
-			return outBuf.Bytes(), err
+	p.ProcessDeenTaskWithFlags = func(flags *flag.FlagSet, task *types.DeenTask) {
+		levelFlag := flags.Lookup("level")
+		level, err := strconv.Atoi(levelFlag.Value.String())
+		if err != nil {
+			task.ErrChan <- err
 		}
-		return outBuf.Bytes(), err
+		if level < bzip2.BestSpeed || level > bzip2.BestCompression {
+			task.ErrChan <- errors.New("Invalid level")
+		}
+		doBZip2Compress(task, level)
+	}
+	p.UnprocessDeenTaskFunc = func(task *types.DeenTask) {
+		defer task.Close()
+		wrappedReader := types.TrimReader{}
+		wrappedReader.Rd = task.Reader
+		decompressor, err := bzip2.NewReader(wrappedReader, nil)
+		if err != nil {
+			task.ErrChan <- err
+		}
+		_, err = io.Copy(task.PipeWriter, decompressor)
+		if err != nil {
+			task.ErrChan <- err
+		}
+	}
+	p.UnprocessDeenTaskWithFlags = func(flags *flag.FlagSet, task *types.DeenTask) {
+		p.UnprocessDeenTaskFunc(task)
+	}
+	p.AddDefaultCliFunc = func(self *types.DeenPlugin, flags *flag.FlagSet, args []string) *flag.FlagSet {
+		flags.Init(p.Name, flag.ExitOnError)
+		flags.Usage = func() {
+			fmt.Fprintf(os.Stderr, "Usage of %s:\n\n", p.Name)
+			fmt.Fprintf(os.Stderr, "BZip2 compressed data format.\n\n")
+			flags.PrintDefaults()
+		}
+		if !self.Unprocess {
+			flags.Int("level", bzip2.DefaultCompression, "compression level from 1 (best speed) to 9 (best compression)")
+		}
+		flags.Parse(args)
+		return flags
 	}
 	return
 }
--- pkg/compressions/bzip2_test.go
+++ pkg/compressions/bzip2_test.go
@@ -3,30 +3,109 @@ package compressions
 import (
 	"bytes"
 	"encoding/hex"
+	"os"
 	"testing"
+
+	"github.com/takeshixx/deen/pkg/helpers"
+	"github.com/takeshixx/deen/pkg/types"
 )
 
-var binData = []byte("4e1260f08e8e1b7d0a829cf82fb23cf95cc1a919cf2187d0276f4c00807faec1")
-var bzipCompressedData = "425a68393141592653591a1c4b54000005fffec0101028208080040005000440008002d0810004002110002000c0004060200031400000000c34610c9a6268f136a9b94e3edcd171788eab42a3ad20964f821aa81f8bb9229c28480d0e25aa00"
+var bzip2TestData = []byte("4e1260f08e8e1b7d0a829cf82fb23cf95cc1a919cf2187d0276f4c00807faec1")
+var bzip2CompressedData = "425a6836314159265359e63fefa100000a89007fe03f002000488a9e794cd4d4c7aa83553f541feaa7ea8fcf50e76c316e577225deee168a1ec6aaa1a5b249a39118f4fcf91a262ba9c84ce177245385090e63fefa10"
+var bzip2CompressedDataLevel1 = "425a6831314159265359e63fefa100000a89007fe03f002000488a9e794cd4d4c7aa83553f541feaa7ea8fcf50e76c316e577225deee168a1ec6aaa1a5b249a39118f4fcf91a262ba9c84ce177245385090e63fefa10"
 
-func TestPluginBzip2Process(t *testing.T) {
-	p := NewPluginBzip2()
-	r := bytes.NewReader(binData)
-	d, e := p.ProcessStreamFunc(r)
-	if e != nil {
-		t.Errorf("Bzip2ProcessStreamFunc failed: %s", e)
+func TestPluginBZip2ProcessDeenTaskFunc(t *testing.T) {
+	destWriter := new(bytes.Buffer)
+	task := types.NewDeenTask(destWriter)
+	task.Reader = bytes.NewReader(bzip2TestData)
+	plugin := NewPluginBzip2()
+	plugin.ProcessDeenTaskFunc(task)
+	compressedData, err := hex.DecodeString(bzip2CompressedData)
+	if err != nil {
+		t.Error(err)
 	}
-	decoded, e := hex.DecodeString(bzipCompressedData)
-	if e != nil {
-		t.Errorf("Decoding test data failed: %s", e)
-	}
-	t.Log(d)
-	t.Log(decoded)
-	if c := bytes.Compare(d, decoded); c != 0 {
-		t.Errorf("Bzip2ProcessSteamFunc returned invalid data: %s", d)
+	select {
+	case err := <-task.ErrChan:
+		t.Error(err)
+	case <-task.DoneChan:
+		if c := bytes.Compare(destWriter.Bytes(), compressedData); c != 0 {
+			t.Errorf("TestPluginBZip2ProcessDeenTaskFunc data wrong: %s != %s", hex.EncodeToString(destWriter.Bytes()), bzip2CompressedData)
+		}
 	}
 }
 
-func TestPluginBzip2Unprocess(t *testing.T) {
+func TestPluginBZip2ProcessDeenTaskWithFlags(t *testing.T) {
+	destWriter := new(bytes.Buffer)
+	task := types.NewDeenTask(destWriter)
+	task.Reader = bytes.NewReader(bzip2TestData)
+	plugin := NewPluginBzip2()
+	flags := helpers.DefaultFlagSet()
+	flags = plugin.AddDefaultCliFunc(&plugin, flags, []string{"-level", "1"})
+	plugin.ProcessDeenTaskWithFlags(flags, task)
+	compressedData, err := hex.DecodeString(bzip2CompressedDataLevel1)
+	if err != nil {
+		t.Error(err)
+	}
+	select {
+	case err := <-task.ErrChan:
+		t.Error(err)
+	case <-task.DoneChan:
+		if c := bytes.Compare(destWriter.Bytes(), compressedData); c != 0 {
+			t.Errorf("TestPluginBZip2ProcessDeenTaskWithFlags data wrong: %s != %s", hex.EncodeToString(destWriter.Bytes()), bzip2CompressedDataLevel1)
+		}
+	}
+}
 
+func TestPluginBZip2UnprocessDeenTaskFunc(t *testing.T) {
+	destWriter := new(bytes.Buffer)
+	task := types.NewDeenTask(destWriter)
+	compressedData, err := hex.DecodeString(bzip2CompressedData)
+	if err != nil {
+		t.Error(err)
+	}
+	task.Reader = bytes.NewReader(compressedData)
+	plugin := NewPluginBzip2()
+	plugin.UnprocessDeenTaskFunc(task)
+	select {
+	case err := <-task.ErrChan:
+		t.Error(err)
+	case <-task.DoneChan:
+		if c := bytes.Compare(destWriter.Bytes(), []byte(bzip2TestData)); c != 0 {
+			t.Errorf("TestPluginBZip2UnprocessDeenTaskFunc data wrong: %s != %s", hex.EncodeToString(destWriter.Bytes()), bzip2TestData)
+		}
+	}
+}
+
+func TestPluginBZip2UnprocessDeenTaskWithFlags(t *testing.T) {
+	destWriter := new(bytes.Buffer)
+	task := types.NewDeenTask(destWriter)
+	compressedData, err := hex.DecodeString(bzip2CompressedData)
+	if err != nil {
+		t.Error(err)
+	}
+	task.Reader = bytes.NewReader(compressedData)
+	plugin := NewPluginBzip2()
+	flags := helpers.DefaultFlagSet()
+	flags = plugin.AddDefaultCliFunc(&plugin, flags, []string{})
+	plugin.UnprocessDeenTaskWithFlags(flags, task)
+	select {
+	case err := <-task.ErrChan:
+		t.Error(err)
+	case <-task.DoneChan:
+		if c := bytes.Compare(destWriter.Bytes(), []byte(bzip2TestData)); c != 0 {
+			t.Errorf("TestPluginBZip2UnprocessDeenTaskWithFlags data wrong: %s != %s", hex.EncodeToString(destWriter.Bytes()), bzip2TestData)
+		}
+	}
+}
+
+func TestPluginBZip2AddDefaultCliFunc(t *testing.T) {
+	p := NewPluginBzip2()
+	flags := helpers.DefaultFlagSet()
+	flags = p.AddDefaultCliFunc(&p, flags, []string{})
+	_, w, err := os.Pipe()
+	if err != nil {
+		t.Error(err)
+	}
+	os.Stderr = w
+	flags.Usage()
 }
