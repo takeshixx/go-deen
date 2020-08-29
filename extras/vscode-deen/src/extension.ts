9164a8c72f155cd8279e3115edeffe08b1e8054c
takeshix <takeshix@adversec.com>
2020-08-30 01:34:46 +0200

    Added all current plugins to vscode extension

--- extras/vscode-deen/src/extension.ts
+++ extras/vscode-deen/src/extension.ts
@@ -1,91 +1,57 @@
 import * as vscode from 'vscode';
 import * as child from 'child_process';
+import { allowedNodeEnvironmentFlags } from 'process';
 
 export function activate(context: vscode.ExtensionContext) {
-
-	context.subscriptions.push(
-		vscode.commands.registerTextEditorCommand('deen.base64Encode', (editor: vscode.TextEditor, edit: vscode.TextEditorEdit) => {
-			let content = editor.document.getText()
-			runDeenPlugin('b64', content)
-		})
-	)
-
-	context.subscriptions.push(
-		vscode.commands.registerTextEditorCommand('deen.base64Decode', (editor: vscode.TextEditor, edit: vscode.TextEditorEdit) => {
-			let content = editor.document.getText()
-			runDeenPlugin('.b64', content)
-		})
-	)
-
-	context.subscriptions.push(
-		vscode.commands.registerTextEditorCommand('deen.base32Encode', (editor: vscode.TextEditor, edit: vscode.TextEditorEdit) => {
-			let content = editor.document.getText()
-			runDeenPlugin('b32', content)
-		})
-	)
-
-	context.subscriptions.push(
-		vscode.commands.registerTextEditorCommand('deen.base32Decode', (editor: vscode.TextEditor, edit: vscode.TextEditorEdit) => {
-			let content = editor.document.getText()
-			runDeenPlugin('.b32', content)
-		})
-	)
-
-	context.subscriptions.push(
-		vscode.commands.registerTextEditorCommand('deen.base85Encode', (editor: vscode.TextEditor, edit: vscode.TextEditorEdit) => {
-			let content = editor.document.getText()
-			runDeenPlugin('b85', content)
-		})
-	)
-
-	context.subscriptions.push(
-		vscode.commands.registerTextEditorCommand('deen.base85Decode', (editor: vscode.TextEditor, edit: vscode.TextEditorEdit) => {
-			let content = editor.document.getText()
-			runDeenPlugin('.b85', content)
-		})
-	)
-
-	context.subscriptions.push(
-		vscode.commands.registerTextEditorCommand('deen.urlEncode', (editor: vscode.TextEditor, edit: vscode.TextEditorEdit) => {
-			let content = editor.document.getText()
-			runDeenPlugin('url', content)
-		})
-	)
-
-	context.subscriptions.push(
-		vscode.commands.registerTextEditorCommand('deen.urlDecode', (editor: vscode.TextEditor, edit: vscode.TextEditorEdit) => {
-			let content = editor.document.getText()
-			runDeenPlugin('.url', content)
-		})
-	)
-
-	context.subscriptions.push(
-		vscode.commands.registerTextEditorCommand('deen.htmlEncode', (editor: vscode.TextEditor, edit: vscode.TextEditorEdit) => {
-			let content = editor.document.getText()
-			runDeenPlugin('html', content)
-		})
-	)
-
-	context.subscriptions.push(
-		vscode.commands.registerTextEditorCommand('deen.htmlDecode', (editor: vscode.TextEditor, edit: vscode.TextEditorEdit) => {
-			let content = editor.document.getText()
-			runDeenPlugin('.html', content)
-		})
-	)
-
-	context.subscriptions.push(
-		vscode.commands.registerTextEditorCommand('deen.jsonFormat', (editor: vscode.TextEditor, edit: vscode.TextEditorEdit) => {
-			let content = editor.document.getText()
-			runDeenPlugin('json', content)
-		})
-	)
-
-	context.subscriptions.push(
-		vscode.commands.registerTextEditorCommand('deen.jsonUnformat', (editor: vscode.TextEditor, edit: vscode.TextEditorEdit) => {
-			let content = editor.document.getText()
-			runDeenPlugin('.json', content)
-		})
-	)
+	
+	addDeenPlugin('b64', 'base64Encode', context)
+	addDeenPlugin('.b64', 'base64Decode', context)
+	addDeenPlugin('b32', 'base32Encode', context)
+	addDeenPlugin('.b32', 'base32Decode', context)
+	addDeenPlugin('b85', 'base85Encode', context)
+	addDeenPlugin('.b85', 'base85Decode', context)
+	addDeenPlugin('url', 'urlEncode', context)
+	addDeenPlugin('.url', 'urlDecode', context)
+	addDeenPlugin('html', 'htmlEncode', context)
+	addDeenPlugin('.html', 'htmlDecode', context)
+	addDeenPlugin('hex', 'hexEncode', context)
+	addDeenPlugin('.hex', 'hexDecode', context)
+	addDeenPlugin('json', 'jsonFormat', context)
+	addDeenPlugin('.json', 'jsonUnformat', context)
+	addDeenPlugin('flate', 'flateCompress', context)
+	addDeenPlugin('.flate', 'flateUncompress', context)
+	addDeenPlugin('lzma', 'lzmaCompress', context)
+	addDeenPlugin('.lzma', 'lzmaUncompress', context)
+	addDeenPlugin('lzma2', 'lzma2Compress', context)
+	addDeenPlugin('.lzma2', 'lzma2Uncompress', context)
+	addDeenPlugin('lzw', 'lzwCompress', context)
+	addDeenPlugin('.lzw', 'lzwUncompress', context)
+	addDeenPlugin('gzip', 'gzipCompress', context)
+	addDeenPlugin('.gzip', 'gzipUncompress', context)
+	addDeenPlugin('zlib', 'zlibCompress', context)
+	addDeenPlugin('.zlib', 'zlibUncompress', context)
+	addDeenPlugin('bzip2', 'bzip2Compress', context)
+	addDeenPlugin('.bzip2', 'bzip2Uncompress', context)
+	addDeenPlugin('brotli', 'brotliCompress', context)
+	addDeenPlugin('.brotli', 'brotliUncompress', context)
+	addDeenPlugin('sha1', 'sha1Hash', context)
+	addDeenPlugin('sha224', 'sha224Hash', context)
+	addDeenPlugin('sha256', 'sha256Hash', context)
+	addDeenPlugin('sha384', 'sha384Hash', context)
+	addDeenPlugin('sha512', 'sha512Hash', context)
+	addDeenPlugin('sha3-224', 'sha3-244Hash', context)
+	addDeenPlugin('sha3-256', 'sha3-256Hash', context)
+	addDeenPlugin('sha3-384', 'sha3-384Hash', context)
+	addDeenPlugin('sha3-512', 'sha3-512Hash', context)
+	addDeenPlugin('md4', 'md4Hash', context)
+	addDeenPlugin('md5', 'md5Hash', context)
+	addDeenPlugin('ripemd160', 'ripemd160Hash', context)
+	addDeenPlugin('blake2s', 'blake2sHash', context)
+	addDeenPlugin('blake2b', 'blake2bHash', context)
+	addDeenPlugin('blake2x', 'blake2xHash', context)
+	addDeenPlugin('blake3', 'blake3Hash', context)
+	addDeenPlugin('bcrypt', 'bcryptHash', context)
+	addDeenPlugin('scrypt', 'scryptHash', context)
 }
 
 
@@ -101,6 +67,15 @@ function getWebviewContent(content: string) {
 	  <pre>${content}</pre>
   </body>
   </html>`;
+}
+
+function addDeenPlugin(plugin: string, vscodeCommand: string, context: vscode.ExtensionContext){
+	context.subscriptions.push(
+		vscode.commands.registerTextEditorCommand('deen.'+vscodeCommand, (editor: vscode.TextEditor, edit: vscode.TextEditorEdit) => {
+			let content = editor.document.getText()
+			runDeenPlugin(plugin, content)
+		})
+	)
 }
 
 function runDeenPlugin(plugin: string, content: string) {
