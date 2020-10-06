79f6d20ce418024ffdc394c6eeaeb12d38f4553f
takeshix <takeshix@adversec.com>
2020-10-06 12:28:06 +0200

    Updated unicode tests

--- pkg/codecs/unicode_test.go
+++ pkg/codecs/unicode_test.go
@@ -61,3 +61,22 @@ func TestPluginUnicodeUsage(t *testing.T) {
 	os.Stderr = w
 	flags.Usage()
 }
+
+var unicodeTestData = []byte{0xfc, 0xc8}
+var unicodeTestDataUTF16 = []byte("주")
+
+/* func TestPluginUnicodeUTF16Process(t *testing.T) {
+	destWriter := new(bytes.Buffer)
+	task := types.NewDeenTask(destWriter)
+	task.Reader = bytes.NewReader(unicodeTestData)
+	plugin := NewPluginUnicode()
+	plugin.ProcessDeenTaskFunc(task)
+	select {
+	case err := <-task.ErrChan:
+		t.Error(err)
+	case <-task.DoneChan:
+		if c := bytes.Compare(destWriter.Bytes(), hexInputDataProcessed); c != 0 {
+			t.Errorf("TestPluginUnicodeUTF16Process data wrong: %s != %s", hex.EncodeToString(destWriter.Bytes()), unicodeTestDataUTF16)
+		}
+	}
+} */
