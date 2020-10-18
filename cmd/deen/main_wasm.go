3a7dfd9c37281dc3914f6005190355568e60237a
takeshix <takeshix@adversec.com>
2020-10-18 13:35:43 +0200

    Added main_wasm.go wrapper

--- cmd/deen/main_wasm.go
+++ cmd/deen/main_wasm.go
@@ -3,16 +3,11 @@
 package main
 
 import (
-	"log"
-
 	"github.com/takeshixx/deen/internal/web"
 )
 
 func main() {
 	// Spawn web interface
 	dw := web.NewDeenWeb()
-	if err := dw.Run(); err != nil {
-		log.Fatal(err)
-	}
-	return
+	dw.Run()
 }
