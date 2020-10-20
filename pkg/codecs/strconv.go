6a9f132b24c770ea30aecd31f86bb81889f2ae6f
takeshix <takeshix@adversec.com>
2020-10-20 23:13:24 +0200

    Added -ctrl flag to strconv plugin
    By default, processing strconv all non-ASCII chars will be escaped.
    Providing `-ctrl` will now allow to only escape control sequences.

--- pkg/codecs/strconv.go
+++ pkg/codecs/strconv.go
@@ -13,6 +13,29 @@ import (
 	"github.com/takeshixx/deen/pkg/types"
 )
 
+func processStrconv(t *types.DeenTask, ctrlOnly bool) {
+	go func() {
+		defer t.Close()
+		str, err := ioutil.ReadAll(t.Reader)
+		if err != nil {
+			t.ErrChan <- errors.Wrap(err, "Failed to read input data")
+		}
+		var quotedStr string
+		if ctrlOnly {
+			quotedStr = strconv.Quote(string(str))
+		} else {
+			quotedStr = strconv.QuoteToASCII(string(str))
+		}
+		quotedStr = strings.TrimPrefix(quotedStr, "\"")
+		quotedStr = strings.TrimSuffix(quotedStr, "\"")
+		strReader := strings.NewReader(quotedStr)
+		_, err = io.Copy(t.PipeWriter, strReader)
+		if err != nil {
+			t.ErrChan <- errors.Wrap(err, "Copying into encoder in strconv failed")
+		}
+	}()
+}
+
 // NewPluginStrconv creates a new PluginStrconv object
 func NewPluginStrconv() (p *types.DeenPlugin) {
 	p = types.NewPlugin()
@@ -21,24 +44,13 @@ func NewPluginStrconv() (p *types.DeenPlugin) {
 	p.Category = "codecs"
 	p.Unprocess = false
 	p.ProcessDeenTaskFunc = func(task *types.DeenTask) {
-		go func() {
-			defer task.Close()
-			str, err := ioutil.ReadAll(task.Reader)
-			if err != nil {
-				task.ErrChan <- errors.Wrap(err, "Failed to read input data")
-			}
-			quotedStr := strconv.QuoteToASCII(string(str))
-			quotedStr = strings.TrimPrefix(quotedStr, "\"")
-			quotedStr = strings.TrimSuffix(quotedStr, "\"")
-			strReader := strings.NewReader(quotedStr)
-			_, err = io.Copy(task.PipeWriter, strReader)
-			if err != nil {
-				task.ErrChan <- errors.Wrap(err, "Copying into encoder in strconv failed")
-			}
-		}()
+		processStrconv(task, false)
 	}
 	p.ProcessDeenTaskWithFlags = func(flags *flag.FlagSet, task *types.DeenTask) {
-		p.ProcessDeenTaskFunc(task)
+		ctrlOnlyPtr := flags.Lookup("ctrl")
+		ctflOnly := false
+		ctflOnly, _ = strconv.ParseBool(ctrlOnlyPtr.Value.String())
+		processStrconv(task, ctflOnly)
 	}
 	p.UnprocessDeenTaskFunc = func(task *types.DeenTask) {
 		go func() {
@@ -69,6 +81,9 @@ func NewPluginStrconv() (p *types.DeenPlugin) {
 			fmt.Fprintf(os.Stderr, "Usage of %s:\n\n", p.Name)
 			fmt.Fprintf(os.Stderr, "Quote/Unquote strings and apply/remove escape characters.\n\n")
 			flags.PrintDefaults()
+		}
+		if !self.Unprocess {
+			flags.Bool("ctrl", false, "only escape control sequences")
 		}
 		flags.Parse(args)
 		return flags
