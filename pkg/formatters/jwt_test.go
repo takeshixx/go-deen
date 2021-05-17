d019f575c71c5eea291d25cc2d931258679f1958
takeshix <takeshix@adversec.com>
2021-05-17 17:29:54 +0200

    Added empty JWT tests

--- pkg/formatters/jwt_test.go
+++ pkg/formatters/jwt_test.go
@@ -1,1 +1,23 @@
 package formatters
+
+import "testing"
+
+func TestNewPluginJwt(t *testing.T) {
+
+}
+
+func TestDecodeToken(t *testing.T) {
+	// Input is a default JWT Token
+}
+
+func TestEncodeToken(t *testing.T) {
+	// Input is a string with a JSON object
+}
+
+func TestNoneSignatureToken(t *testing.T) {
+	// Input should be a default token, output should set alg to "none"
+}
+
+func TestDecodeOutputFormats(t *testing.T) {
+	// Take a default token, output in all 3 supported formats
+}
