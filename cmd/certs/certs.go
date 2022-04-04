df2ba3137b215b1310971ea5cf7f8491654243be
takeshix <takeshix@adversec.com>
2022-04-04 23:25:44 +0200

    Updated the initial testing script

--- cmd/certs/certs.go
+++ cmd/certs/certs.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
 	"encoding/pem"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
 )
 
@@ -13,6 +19,129 @@ func PrintCert(certPEM []byte) (err error) {
 	fmt.Printf("rawCert: %v\n", rawCert)
 	return
 }
+
+func ParseCert(certPEM []byte) (err error) {
+	block, _ := pem.Decode(certPEM)
+	fmt.Printf("Cert type: %s\n", block.Type)
+	cert, err := x509.ParseCertificate(block.Bytes)
+	if err != nil {
+		return
+	}
+	// Issuer and Subject
+	fmt.Printf("Issuer: %s\n", cert.Issuer)
+	fmt.Printf("Subject: %s\n", cert.Subject)
+	fmt.Printf("Public key algorithm: %s\n", cert.PublicKeyAlgorithm)
+	fmt.Printf("Policy Identifiers: %v\n", cert.PolicyIdentifiers)
+	fmt.Printf("SubjectKeyId: %v\n", cert.SubjectKeyId)
+
+	var newPrivateKey, newPublicKey interface{}
+
+	switch cert.PublicKeyAlgorithm {
+	case x509.RSA:
+		fmt.Printf("Creating RSA key with size: %d\n", cert.PublicKey.(*rsa.PublicKey).Size()*8)
+		newPrivateKey, err = rsa.GenerateKey(rand.Reader, cert.PublicKey.(*rsa.PublicKey).Size()*8)
+		if err != nil {
+			return
+		}
+		newPublicKey = &newPrivateKey.(*rsa.PrivateKey).PublicKey
+	case x509.ECDSA:
+		newPrivateKey, err = ecdsa.GenerateKey(cert.PublicKey.(elliptic.Curve), rand.Reader)
+		if err != nil {
+			return
+		}
+		newPublicKey = &newPrivateKey.(*ecdsa.PrivateKey).PublicKey
+	case x509.DSA:
+	case x509.Ed25519:
+		fmt.Printf("Public key algorithm %s not implemented yet.\n", cert.PublicKeyAlgorithm)
+	}
+
+	newCert := cert
+	newCert.ExtraExtensions = newCert.Extensions
+	newCert.PublicKey = newPublicKey
+	fmt.Printf("newCert subject: %v\n", newCert.Subject)
+	fmt.Printf("newCert issuer: %v\n", newCert.Issuer)
+
+	issuerCert := *cert
+	issuerCert.ExtraExtensions = issuerCert.Extensions
+	issuerCert.Subject = cert.Issuer
+	issuerCert.RawSubject = cert.RawIssuer
+	fmt.Printf("CA cert subject: %v\n", issuerCert.Subject)
+	fmt.Printf("CA cert issuer: %v\n", issuerCert.Issuer)
+
+	_ = &x509.Certificate{
+		SerialNumber:                cert.SerialNumber,
+		SignatureAlgorithm:          cert.SignatureAlgorithm,
+		PublicKeyAlgorithm:          cert.PublicKeyAlgorithm,
+		PublicKey:                   newPublicKey,
+		Version:                     cert.Version,
+		Subject:                     cert.Subject,
+		RawSubject:                  cert.RawSubject,
+		Issuer:                      cert.Issuer,
+		RawIssuer:                   cert.RawIssuer,
+		NotBefore:                   cert.NotBefore,
+		NotAfter:                    cert.NotAfter,
+		SubjectKeyId:                cert.SubjectKeyId,
+		ExtKeyUsage:                 cert.ExtKeyUsage,
+		KeyUsage:                    cert.KeyUsage,
+		Extensions:                  cert.Extensions,
+		ExtraExtensions:             cert.ExtraExtensions,
+		UnhandledCriticalExtensions: cert.UnhandledCriticalExtensions,
+		UnknownExtKeyUsage:          cert.UnknownExtKeyUsage,
+		BasicConstraintsValid:       cert.BasicConstraintsValid,
+		IsCA:                        cert.IsCA,
+		MaxPathLen:                  cert.MaxPathLen,
+		MaxPathLenZero:              cert.MaxPathLenZero,
+		OCSPServer:                  cert.OCSPServer,
+		IssuingCertificateURL:       cert.IssuingCertificateURL,
+		DNSNames:                    cert.DNSNames,
+		EmailAddresses:              cert.EmailAddresses,
+		IPAddresses:                 cert.IPAddresses,
+		URIs:                        cert.URIs,
+		PermittedDNSDomainsCritical: cert.PermittedDNSDomainsCritical,
+		PermittedDNSDomains:         cert.PermittedDNSDomains,
+		ExcludedDNSDomains:          cert.ExcludedDNSDomains,
+		PermittedIPRanges:           cert.PermittedIPRanges,
+		PermittedEmailAddresses:     cert.PermittedEmailAddresses,
+		ExcludedEmailAddresses:      cert.ExcludedEmailAddresses,
+		PermittedURIDomains:         cert.PermittedURIDomains,
+		ExcludedURIDomains:          cert.ExcludedURIDomains,
+		CRLDistributionPoints:       cert.CRLDistributionPoints,
+		PolicyIdentifiers:           cert.PolicyIdentifiers,
+	}
+	//fmt.Printf("New cert Subject: %s\n", newCert.Subject)
+	//fmt.Printf("New private key: %v\n", newPrivateKey)
+
+	fmt.Println("Creating certificate")
+
+	derBytes, err := x509.CreateCertificate(rand.Reader, newCert, &issuerCert, newPublicKey, newPrivateKey)
+	if err != nil {
+		return
+	}
+
+	fmt.Println("Creating output file")
+
+	certOut, err := os.Create("/tmp/certs/newCert2")
+	if err != nil {
+		return
+	}
+
+	fmt.Println("Writing newCert to file")
+
+	if err = pem.Encode(certOut, &pem.Block{Type: "CERTIFICATE", Bytes: derBytes}); err != nil {
+		return
+	}
+
+	if err = certOut.Close(); err != nil {
+		return
+	}
+
+	return
+}
+
+/* func SaveCertToPath(x509.Certificate cert, string path) (err error) {
+
+	return
+} */
 
 func main() {
 	if len(os.Args) < 2 {
@@ -32,7 +161,11 @@ func main() {
 		return
 	}
 
-	fmt.Printf("Read certificate data:\n\n%s\n", certData)
+	//fmt.Printf("Read certificate data:\n\n%s\n", certData)
 
-	PrintCert([]byte(certData))
+	//PrintCert([]byte(certData))
+	err = ParseCert([]byte(certData))
+	if err != nil {
+		log.Fatal(err)
+	}
 }
